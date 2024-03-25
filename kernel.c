e019576d9ab24c4536dd9a80b89758f36356289b
Anthony <makeanthonygreatagain@gmail.com>
2024-03-25 11:09:56 -0400

    Finished void clear_terminal()

--- kernel.c
+++ kernel.c
@@ -2,12 +2,17 @@
 /* Kernel terminal is 80x25. */
 const int SCREEN_WIDTH = 80;
 const int SCREEN_HEIGHT = 25;
+char* const VGA_BUFFER_POS = (char*) 0xb8000;
+
+void clear_terminal();
 
 void main() {
 	
+	clear_terminal();
+
 	/* 60 times a second, we pull the information from memory
 	 * 	and paint it on the screen. */
-	char* const VGA_BUFFER = (char*) 0xb8000;
+	char* const VGA_BUFFER = VGA_BUFFER_POS;
 	char* str = "Hello World";
 	
 	/* Basically how you print a string. */
@@ -18,3 +23,9 @@ void main() {
 	return;
 }
 
+void clear_terminal() {
+	char* const VGA_BUFFER = VGA_BUFFER_POS;
+	for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++) {
+		VGA_BUFFER[i*2] = (char) 0x20;
+	}
+}
