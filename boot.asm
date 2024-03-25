c0b6b56b3528670ad4aac9f35d6a06b3792cb834
Anthony <makeanthonygreatagain@gmail.com>
2024-03-25 10:45:40 -0400

    Added comments from 25 Mar. 2024 Lecture

--- boot.asm
+++ boot.asm
@@ -1,8 +1,8 @@
-[bits 16] / 16 bytes from the end
-[org 0x7c00]
+[bits 16] 	/ 16-bit mode
+[org 0x7c00] 	/ Bootloader is put in 0x7c00
 
-KERNEL_ADDRESS equ 0x1000
-mov [BOOT_DRIVE], DL
+KERNEL_ADDRESS equ 0x1000	; Kernel loaded on 2nd page
+mov [BOOT_DRIVE], DL		; 
 mov bp, 0x9000
 mov sp, bp
 
@@ -16,30 +16,31 @@ jmp done
 
 kernel_load:
 	pusha
-	mov AH, 0x02
-	mov AL, [NUM_SECTORS]
-	mov BX, KERNEL_ADDRESS
-	mov CH, 0x00
-	mov CL, 0x02
-	mov DH, 0x00
-	mov DL, [BOOT_DRIVE]
-	int 13h
-	jc disk_read_error
-	cmp AL, [NUM_SECTORS]
+	mov AH, 0x02		; Read mode
+	mov AL, [NUM_SECTORS]	; Loads NUM_SECTORS into AL
+	mov BX, KERNEL_ADDRESS	; Loads kernel address into BX
+	mov CH, 0x00		; CH - Cylinder
+	mov CL, 0x02		; Sector 2
+	mov DH, 0x00		; Head
+	mov DL, [BOOT_DRIVE]	; (???)
+	int 13h			; (???)
+	jc disk_read_error	; If there is a flag, jump to disk_read_error
+	cmp AL, [NUM_SECTORS]	; Compare number of sectors requested
+				; 	to number of sectors read
 	jne disk_sector_error
-	popa
+	popa			
 	ret
 disk_read_error:
 	mov BX, DISK_READ_ERROR_MESSAGE
 	call print_string
-	jmp done
+	jmp done			; Keeps jumping to the same address
 disk_sector_error:
 	mov BX, DISK_SECTOR_ERROR_MESSAGE
 	call print_string
 	jmp done
 
 
-print_string:
+print_string:	; Prints characters one by one
 	pusha
 	mov AH, 0x0e
 print_loop:
@@ -57,15 +58,19 @@ done:
 	jmp $
 
 
-switch_to_32bit:
+switch_to_32bit:	; Initializes GDT
+			; 64 individual bits that need to be set
 	cli
 	lgdt [gdt]
+			; Sets control register
 	mov EAX, CR0
-	or eax, 0x01
+	or eax, 0x01	; Turns on bit 1 to mark 32-bit mode.
 	mov CR0, EAX
-	jmp 0x08:begin_32bit
+			;Cannot manupulate CR0 directly
 
-[bits 32]
+	jmp 0x08:begin_32bit	; Far jump
+
+[bits 32] / 32-bit mode
 begin_32bit:
 	mov AX, 0x10
 	mov SS, AX
@@ -73,16 +78,15 @@ begin_32bit:
 	mov ES, AX
 	mov FS, AX
 	mov GS, AX
-	mov ebp, 0x90000
+	mov ebp, 0x90000	; Higher stack
 	mov esp, ebp
-	jmp KERNEL_ADDRESS
-
+	jmp KERNEL_ADDRESS	; Jumps to kernel address in kernel-entry.elf
 
 NUM_SECTORS db 0x09
 DISK_SUCCESS_MESSAGE db "Read succeeded, continuing", 0
 DISK_READ_ERROR_MESSAGE db "Problem with disk read", 0
 DISK_SECTOR_ERROR_MESSAGE db "Read and requested sectors differ", 0
 BOOT_DRIVE db 0
-times 510 - ($-$$) db 0
-dw 0xaa55
+times 510 - ($-$$) db 0	; Pads unused bytes up to 510th
+dw 0xaa55		; Magic bytes
 
--- kernel.c
+++ kernel.c
@@ -1,11 +1,20 @@
-void main() {
 
+/* Kernel terminal is 80x25. */
+const int SCREEN_WIDTH = 80;
+const int SCREEN_HEIGHT = 25;
+
+void main() {
+	
+	/* 60 times a second, we pull the information from memory
+	 * 	and paint it on the screen. */
 	char* const VGA_BUFFER = (char*) 0xb8000;
 	char* str = "Hello World";
-
+	
+	/* Basically how you print a string. */
 	for (int i=0; str[i] != '\0'; i++) {
 		VGA_BUFFER[i*2] = str[i];
 	}
 
 	return;
 }
+
