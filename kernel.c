64b9a8aaeb917981bb907ba4277fe37c4b5374a4
Anthony <makeanthonygreatagain@gmail.com>
2024-03-25 12:26:00 -0400

    Console functions written but not debugged yet.

--- kernel.c
+++ kernel.c
@@ -4,28 +4,43 @@ const int SCREEN_WIDTH = 80;
 const int SCREEN_HEIGHT = 25;
 char* const VGA_BUFFER_POS = (char*) 0xb8000;
 
+/*
 void clear_terminal();
+*/
 
 void main() {
 	
 	clear_terminal();
+	print_string("HELLO");
+	print_line("WORLD");
+	print_string("TODAY");
+
+	/*	
+	clear_terminal();
+	*/
 
 	/* 60 times a second, we pull the information from memory
 	 * 	and paint it on the screen. */
+	/*
 	char* const VGA_BUFFER = VGA_BUFFER_POS;
 	char* str = "Hello World";
-	
+	*/
+
 	/* Basically how you print a string. */
+	/*
 	for (int i=0; str[i] != '\0'; i++) {
 		VGA_BUFFER[i*2] = str[i];
 	}
+	*/
 
 	return;
 }
 
+/*
 void clear_terminal() {
 	char* const VGA_BUFFER = VGA_BUFFER_POS;
 	for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++) {
 		VGA_BUFFER[i*2] = (char) 0x20;
 	}
 }
+*/
---
+++ shell/console.c
@@ -0,0 +1,60 @@
+#include "console.h"
+
+char* const VGA_BUFFER_POS = (char* 0xb8000);
+char const SPACE = (char) 0x20;
+
+int terminal_pos = 0;
+
+char* get_address();
+
+void clear_terminal() {
+	char* const VGA_BUFFER = VGA_BUFFER_POS;
+	for(int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+		VGA_BUFFER[i * VGA_BYTES_PER_CHARACTER] = SPACE;
+	}
+	return;
+}
+
+void print_character(char c) {
+	char* address = get_address();
+	/* Suggested to use a switch statement to find the newline character. */
+	switch (c) {
+		case '\n': 
+			terminal_pos = get_next_line();
+			break;
+		default: 
+			/* Check if a character is printable. */
+			if((c > (char)(0x1f)) && (c < (char)(0x7f))) {
+				address = c;
+			}	
+			terminal_pos++; 
+	}
+	return;
+}
+
+void print_string(char* str) {
+	char* current_pos;
+	for(int i = 0; str[i] != '\0'; i++) {
+		current_pos = get_address();
+		*current_pos = str[i]
+		terminal_pos++;
+	}
+	return;
+}
+
+void print_line(char* str) {
+	print_string(str);
+	print_char('\n');
+}
+
+/* Gets the address of the character to be written to. */
+char* get_address() {
+	return (char*) (VGA_BUFFER_POS + terminal_pos * 
+				VGA_BYTES_PER_CHARACTER);
+}
+
+/* Gets the terminal position for the next line. */
+int get_next_line() {
+	/* Uses integer division to get the next higher multiple of 80. */
+	return (terminal_pos + 80) / 80 * 80;
+}
