40600b2dd6e8162ee3e1bd821edb2b479b8bca2e
Anthony <makeanthonygreatagain@gmail.com>
2024-04-25 11:15:19 -0400

    Keyboard input works as intended

--- device/portmap/portmap.c
+++ device/portmap/portmap.c
@@ -1,6 +1,8 @@
 #include "portmap.h"
 #include "stdint.h"
 
+/* Inline asm is bad practice. Instead, make a procedure call to a .s file. */
+
 uint8_t inb(uint16_t port) {
      uint8_t ret;
      __asm__ __volatile__("inb %1, %0":"=a"(ret):"Nd"(port));
